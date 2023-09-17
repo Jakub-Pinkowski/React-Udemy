2046b77ac1a1bf5c73620f475d70dc49ed4a5cf8
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-09-17 17:03:27 +0200

    Finished module 90

--- Section_2/my-app/src/components/Expenses/Expenses.js
+++ Section_2/my-app/src/components/Expenses/Expenses.js
@@ -4,7 +4,7 @@ import './Expenses.css'
 
 import ExpensesFilter from './ExpensesFilter'
 import Card from '../UI/Card'
-import ExpenseItem from './ExpenseItem'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020')
@@ -17,19 +17,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear
     })
 
-    let expensesContent = <p>No expenses found.</p>
-
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
-
     return (
         <div>
             <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
                     selectedYear={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
