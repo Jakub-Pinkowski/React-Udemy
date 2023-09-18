f9b798f85ce990ea3bec34037602a9938f1bc743
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-09-18 19:57:10 +0200

    Finished module 94

--- Section_2/my-app/src/components/NewExpense/ExpenseForm.js
+++ Section_2/my-app/src/components/NewExpense/ExpenseForm.js
@@ -24,7 +24,7 @@ const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate),
         }
 
@@ -73,7 +73,7 @@ const ExpenseForm = (props) => {
                 </div>
             </div>
             <div className="new-expense__actions">
-                <button onClick={cancelHandler}>Cancel</button> 
+                <button onClick={cancelHandler}>Cancel</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
