968342fa708c3a702d48a5261b8e6b2cb4a6fb07
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-09-20 20:26:16 +0200

    Finished module 102

--- Section_7/src/components/UI/Button/Button.js
+++ Section_7/src/components/UI/Button/Button.js
@@ -1,8 +1,12 @@
-import './Button.css'
+import classes from './Button.module.css'
 
 const Button = (props) => {
     return (
-        <button type={props.type} className="button" onClick={props.onClick}>
+        <button
+            className={classes.button}
+            type={props.type || 'button'}
+            onClick={props.onClick}
+        >
             {props.children}
         </button>
     )
