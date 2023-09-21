a7b2d9750661f6a01252e1375f0ca74fc634191c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-09-21 21:37:18 +0200

    Separated Form

--- Section_9/src/App.js
+++ Section_9/src/App.js
@@ -1,5 +1,3 @@
-import logo from './assets/investment-calculator-logo.png'
-
 import Header from './components/Header'
 import Results from './components/Results'
 import Form from './components/Form'
@@ -35,45 +33,7 @@ function App() {
     return (
         <div>
             <Header />
-
-            <form className="form">
-                <div className="input-group">
-                    <p>
-                        <label htmlFor="current-savings">
-                            Current Savings ($)
-                        </label>
-                        <input type="number" id="current-savings" />
-                    </p>
-                    <p>
-                        <label htmlFor="yearly-contribution">
-                            Yearly Savings ($)
-                        </label>
-                        <input type="number" id="yearly-contribution" />
-                    </p>
-                </div>
-                <div className="input-group">
-                    <p>
-                        <label htmlFor="expected-return">
-                            Expected Interest (%, per year)
-                        </label>
-                        <input type="number" id="expected-return" />
-                    </p>
-                    <p>
-                        <label htmlFor="duration">
-                            Investment Duration (years)
-                        </label>
-                        <input type="number" id="duration" />
-                    </p>
-                </div>
-                <p className="actions">
-                    <button type="reset" className="buttonAlt">
-                        Reset
-                    </button>
-                    <button type="submit" className="button">
-                        Calculate
-                    </button>
-                </p>
-            </form>
+            <Form onCalculate={calculateHandler} />
 
             {/* Todo: Show below table conditionally (only once result data is available) */}
             {/* Show fallback text if no data is available */}
--- Section_9/src/components/Form.js
+++ Section_9/src/components/Form.js
@@ -1,5 +1,44 @@
 import classes from './Form.module.css'
 
-const Form = (props) => {}
+const Form = () => {
+    return (
+        <form className={classes.form}>
+            <div className={classes['input-group']}>
+                <p>
+                    <label htmlFor="current-savings">Current Savings ($)</label>
+                    <input type="number" id="current-savings" />
+                </p>
+                <p>
+                    <label htmlFor="yearly-contribution">
+                        Yearly Savings ($)
+                    </label>
+                    <input type="number" id="yearly-contribution" />
+                </p>
+            </div>
+            <div className={classes['input-group']}>
+                <p>
+                    <label htmlFor="expected-return">
+                        Expected Interest (%, per year)
+                    </label>
+                    <input type="number" id="expected-return" />
+                </p>
+                <p>
+                    <label htmlFor="duration">
+                        Investment Duration (years)
+                    </label>
+                    <input type="number" id="duration" />
+                </p>
+            </div>
+            <p className={classes.actions}>
+                <button type="reset" className={classes.buttonAlt}>
+                    Reset
+                </button>
+                <button type="submit" className={classes.button}>
+                    Calculate
+                </button>
+            </p>
+        </form>
+    )
+}
 
 export default Form
