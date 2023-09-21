7a3276deac1f9d3b3837ad8a3575da2cd176aa91
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-09-21 21:55:11 +0200

    Implemented form submission with binded values

--- Section_9/src/components/Form.js
+++ Section_9/src/components/Form.js
@@ -1,15 +1,21 @@
+import { useState } from 'react'
+
 import classes from './Form.module.css'
 
 const Form = (props) => {
+    const [currentSavings, setCurrentSavings] = useState('')
+    const [yearlyContribution, setYearlyContribution] = useState('')
+    const [expectedReturn, setExpectedReturn] = useState('')
+    const [duration, setDuration] = useState('')
+
     const submitHandler = (event) => {
         event.preventDefault()
 
         const userInput = {
-            // feel free to change the shape of this object!
-            'current-savings': event.target[0].value,
-            'yearly-contribution': event.target[1].value,
-            'expected-return': event.target[2].value,
-            duration: event.target[3].value,
+            'current-savings': currentSavings,
+            'yearly-contribution': yearlyContribution,
+            'expected-return': expectedReturn,
+            duration: duration,
         }
 
         console.log(userInput)
@@ -20,8 +26,10 @@ const Form = (props) => {
     const resetHandler = (event) => {
         event.preventDefault()
 
-        // Access the form element and call the reset() method
-        event.target.closest('form').reset()
+        setCurrentSavings('')
+        setYearlyContribution('')
+        setExpectedReturn('')
+        setDuration('')
     }
 
     return (
@@ -29,13 +37,27 @@ const Form = (props) => {
             <div className={classes['input-group']}>
                 <p>
                     <label htmlFor="current-savings">Current Savings ($)</label>
-                    <input type="number" id="current-savings" />
+                    <input
+                        type="number"
+                        id="current-savings"
+                        value={currentSavings}
+                        onChange={(event) =>
+                            setCurrentSavings(event.target.value)
+                        }
+                    />
                 </p>
                 <p>
                     <label htmlFor="yearly-contribution">
                         Yearly Savings ($)
                     </label>
-                    <input type="number" id="yearly-contribution" />
+                    <input
+                        type="number"
+                        id="yearly-contribution"
+                        value={yearlyContribution}
+                        onChange={(event) =>
+                            setYearlyContribution(event.target.value)
+                        }
+                    />
                 </p>
             </div>
             <div className={classes['input-group']}>
@@ -43,13 +65,25 @@ const Form = (props) => {
                     <label htmlFor="expected-return">
                         Expected Interest (%, per year)
                     </label>
-                    <input type="number" id="expected-return" />
+                    <input
+                        type="number"
+                        id="expected-return"
+                        value={expectedReturn}
+                        onChange={(event) =>
+                            setExpectedReturn(event.target.value)
+                        }
+                    />
                 </p>
                 <p>
                     <label htmlFor="duration">
                         Investment Duration (years)
                     </label>
-                    <input type="number" id="duration" />
+                    <input
+                        type="number"
+                        id="duration"
+                        value={duration}
+                        onChange={(event) => setDuration(event.target.value)}
+                    />
                 </p>
             </div>
             <p className={classes.actions}>
