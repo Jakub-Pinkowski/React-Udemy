3eeb3ca8a37665cc67a356f23fb1ecfb8d55afdb
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-03 16:12:50 +0200

    Finished module 186

--- Section_13/src/App.js
+++ Section_13/src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
 
 import './App.css'
+import Button from './components/UI/Button/Button'
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [shawParagraph, setShowParagraph] = useState(false)
+
+    const toggleParagraphHandler = () => {
+        setShowParagraph((prevShowParagraph) => !prevShowParagraph)
+    }
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
+            {shawParagraph && <p>This is new!</p>}
+            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     )
 }
