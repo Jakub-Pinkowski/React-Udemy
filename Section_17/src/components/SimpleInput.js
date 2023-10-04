e95d4e6bb335295a8b45d3fa53add5c621da94f0
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-04 23:08:05 +0200

    Finished module 235

--- Section_17/src/components/SimpleInput.js
+++ Section_17/src/components/SimpleInput.js
@@ -1,7 +1,9 @@
 import { useRef, useState } from 'react'
 
 const SimpleInput = (props) => {
+    const nameInputRef = useRef()
     const [enteredName, setEnteredName] = useState('')
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
 
     const nameInputChangeHandler = (event) => {
         setEnteredName(event.target.value)
@@ -11,17 +13,22 @@ const SimpleInput = (props) => {
         event.preventDefault()
 
         if (enteredName.trim() === '') {
+            setEnteredNameIsValid(false)
             return
         }
 
+        setEnteredNameIsValid(true)
+
         console.log(enteredName)
 
-        setEnteredName('') // IDEAL
+        setEnteredName('')
     }
+
+    const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
 
     return (
         <form onSubmit={formSubmissionHandler} className="form-control">
-            <div className="form-control">
+            <div className={nameInputClasses}>
                 <label htmlFor="name">Your Name</label>
                 <input
                     type="text"
@@ -29,6 +36,7 @@ const SimpleInput = (props) => {
                     onChange={nameInputChangeHandler}
                     value={enteredName}
                 />
+                {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
             </div>
             <div className="form-actions">
                 <button>Submit</button>
