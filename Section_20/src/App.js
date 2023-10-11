16ea95f5ffc5eb10afc07ce1283628b37a0479ae
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-11 22:38:11 +0200

    Finished module 291

--- Section_20/src/App.js
+++ Section_20/src/App.js
@@ -1,11 +1,24 @@
 import { useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
 
+const firebaseUrl = 'https://react-counter-64e25-default-rtdb.europe-west1.firebasedatabase.app/'
+
 function App() {
     const showCart = useSelector((state) => state.ui.cartIsVisible)
+    const cart = useSelector((state) => state.cart)
+
+    useEffect(() => {
+        const sendCartData = async () => {
+            const response = await axios.put(firebaseUrl + 'cart.json', cart)
+            console.log(response)
+        }
+        sendCartData()
+    }, [cart])
 
     return (
         <Layout>
