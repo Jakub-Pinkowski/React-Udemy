202c5709b508c85d590b67797f9b34814b4273c5
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-12 22:41:00 +0200

    Finished module 310

--- Section_21/react-router/src/pages/Home.js
+++ Section_21/react-router/src/pages/Home.js
@@ -1,11 +1,22 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function HomePage() {
+    const navigate = useNavigate()
+
+    function navigateHandler() {
+        navigate('/products')
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
             <p>
                 <Link to="/products">View Products</Link>
+            </p>
+            <p>
+                <button type="button" onClick={navigateHandler}>
+                    Products
+                </button>
             </p>
         </div>
     )
