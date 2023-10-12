58f3e3b740e9b22c012b2b63e7e8ed731db91147
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-12 23:58:00 +0200

    Working on the assignment

--- Section_21_b/frontend/src/pages/EventDetailPage.js
+++ Section_21_b/frontend/src/pages/EventDetailPage.js
@@ -1,7 +1,12 @@
+import { useParams } from 'react-router-dom'
+
 const EventDetailPage = () => {
+    const { eventId } = useParams()
+
     return (
         <div>
             <h1>Event Detail Page</h1>
+            <p>{eventId}</p>
         </div>
     )
 }
--- Section_21_b/frontend/src/pages/EventsPage.js
+++ Section_21_b/frontend/src/pages/EventsPage.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 const DUMMY_EVENTS = [
     {
         id: 'e1',
@@ -34,7 +36,9 @@ const EventsPage = () => {
             <h1>Events Page</h1>
             <ul>
                 {DUMMY_EVENTS.map((event) => (
-                    <li key={event.id}>{event.title}</li>
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.title}</Link>
+                    </li>
                 ))}
             </ul>
         </div>
