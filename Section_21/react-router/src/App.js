fe4836e671f12a339e43613801c81a01afa63c93
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-12 22:10:00 +0200

    Finished module 307

--- Section_21/react-router/src/App.js
+++ Section_21/react-router/src/App.js
@@ -1,20 +1,24 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import RootLayout from './pages/Root'
 import HomePage from './pages/Home'
 import Products from './pages/Products'
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <HomePage />,
+        element: <RootLayout />,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />,
+            },
+            {
+                path: '/products',
+                element: <Products />,
+            },
+        ],
     },
-    {
-        path: '/products',
-        element: <Products />,
-    },
-    {
-
-    }
 ])
 
 function App() {
