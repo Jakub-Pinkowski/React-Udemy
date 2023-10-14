7a35509dcd7825cb99e2a2b052cc3b82c466e17d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-14 19:23:34 +0200

    Working on defer logic

--- Section_21_b/frontend/src/pages/EventsPage.js
+++ Section_21_b/frontend/src/pages/EventsPage.js
@@ -1,27 +1,36 @@
-import { useLoaderData, json } from 'react-router-dom'
+import { Suspense } from 'react'
+import { useLoaderData, json, defer, Await } from 'react-router-dom'
 import EventsList from '../components/EventsList'
 
-const eventsUlr = 'http://localhost:8080/events'
+const eventsUrl = 'http://localhost:8080/events'
 
 function EventsPage() {
-    const data = useLoaderData()
-    const events = data.events
+    const { events } = useLoaderData()
+
+    console.log(events)
 
     return (
-        <>
-            <EventsList events={events} />
-        </>
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
+        </Suspense>
     )
 }
 
 export default EventsPage
 
-export async function loader() {
-    const response = await fetch(eventsUlr)
+async function loadEvents() {
+    const response = await fetch(eventsUrl)
 
-    if (response.status !== 200) {
+    if (!response.ok) {
         throw json({ message: 'Could not fetch events' }, { status: 500 })
     } else {
-        return response
+        const resData = await response.json()
+        return resData
     }
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents(),
+    })
 }
