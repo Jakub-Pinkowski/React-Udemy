91c19f23f1cfaf359baab30737f264564124b16d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-14 19:43:52 +0200

    Started module 339

--- Section_21_b/frontend/src/pages/EventDetailPage.js
+++ Section_21_b/frontend/src/pages/EventDetailPage.js
@@ -1,6 +1,7 @@
 import { useRouteLoaderData, json, redirect } from 'react-router-dom'
 
 import EventItem from '../components/EventItem'
+import EventsList from '../components/EventsList'
 
 const eventsUlr = 'http://localhost:8080/events'
 
@@ -8,12 +9,17 @@ const EventDetailPage = () => {
     const data = useRouteLoaderData('event-detail')
     const event = data.event
 
-    return <EventItem event={event} />
+    return (
+        <>
+            <EventItem event={event} />
+            <EventsList events={data.events} />
+        </>
+    )
 }
 
 export default EventDetailPage
 
-export async function loader({ request, params }) {
+async function loadEvent(id) {
     const id = params.eventId
 
     const response = await fetch(`${eventsUlr}/${id}`)
@@ -21,7 +27,21 @@ export async function loader({ request, params }) {
     if (!response.ok) {
         throw json({ message: 'Could not fetch event' }, { status: 500 })
     } else {
-        return response
+        const resData = await response.json()
+        const event = resData.event
+        return event
+    }
+}
+
+async function loadEvents() {
+    const response = await fetch(eventsUrl)
+
+    if (!response.ok) {
+        throw json({ message: 'Could not fetch events' }, { status: 500 })
+    } else {
+        const resData = await response.json()
+        const events = resData.events
+        return events
     }
 }
 
