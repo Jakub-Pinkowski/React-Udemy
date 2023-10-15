91f1dd783b358a26d82443f5de69091a3cf9ca3b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-15 17:16:20 +0200

    Aeed new consts

--- Section_24/src/components/Events/EditEvent.jsx
+++ Section_24/src/components/Events/EditEvent.jsx
@@ -1,10 +1,31 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
+import { useQuery, useMutation } from '@tanstack/react-query'
 
 import Modal from '../UI/Modal.jsx'
 import EventForm from './EventForm.jsx'
+import { fetchEvent, updateEvent } from '../../util/http.js'
 
 export default function EditEvent() {
+    const { id } = useParams()
     const navigate = useNavigate()
+
+    const { data, isPending, isError, error } = useQuery({
+        queryKey: ['event', id],
+        queryFn: ({ signal }) =>
+            fetchEvent({
+                signal,
+                id,
+            }),
+    })
+
+    const {
+        mutate,
+        isPending: isPendingUpdate,
+        isError: isErrorUpdate,
+        error: updateError,
+    } = useMutation({
+        mutationFn: updateEvent,
+    })
 
     function handleSubmit(formData) {}
 
