913917fb4896085db4e1772ed4c772c11a0c2f5a
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-15 17:20:28 +0200

    Added contect

--- Section_24/src/components/Events/EditEvent.jsx
+++ Section_24/src/components/Events/EditEvent.jsx
@@ -3,6 +3,8 @@ import { useQuery, useMutation } from '@tanstack/react-query'
 
 import Modal from '../UI/Modal.jsx'
 import EventForm from './EventForm.jsx'
+import LoadingIndicator from '../UI/LoadingIndicator.jsx'
+import ErrorBLock from '../UI/ErrorBlock.jsx'
 import { fetchEvent, updateEvent } from '../../util/http.js'
 
 export default function EditEvent() {
@@ -27,15 +29,46 @@ export default function EditEvent() {
         mutationFn: updateEvent,
     })
 
-    function handleSubmit(formData) {}
+    function handleSubmit(formData) {
+        mutate({
+            id,
+            data: formData,
+        })
+    }
 
     function handleClose() {
         navigate('../')
     }
 
+    let content
+
+    if (isPending) {
+        content = (
+            <div className="center">
+                <LoadingIndicator />
+            </div>
+        )
+    }
+
+    if (isError) {
+        content = (
+            <>
+                <ErrorBLock
+                    title="Failed to load event"
+                    message={error.info?.message || 'Please try again later'}
+                />
+                <div lassName="form-actions">
+                    <Link to="../" className="button">
+                        Okay
+                    </Link>
+                </div>
+            </>
+        )
+    }
+
     return (
         <Modal onClose={handleClose}>
-            <EventForm inputData={null} onSubmit={handleSubmit}>
+            <EventForm inputData={data} onSubmit={handleSubmit}>
                 <Link to="../" className="button-text">
                     Cancel
                 </Link>
