28da84994fb97700a4224045e7cd92ffc9e38305
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-16 19:22:26 +0200

    Finished module 404

--- Section_25/udemy-next-1/pages/index.js
+++ Section_25/udemy-next-1/pages/index.js
@@ -4,14 +4,14 @@ const DUMMY_MEETUPS = [
     {
         id: 'm1',
         title: 'A First Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Chicago_from_North_Avenue_Beach_June_2015_panorama_2.jpg/1280px-Chicago_from_North_Avenue_Beach_June_2015_panorama_2.jpg',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Belgium.svg/1200px-Flag_of_Belgium.svg.png',
         address: 'Some address 5, 12345 Some City',
         description: 'This is a first meetup!',
     },
     {
         id: 'm2',
         title: 'A Second Meetup',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Chicago_from_North_Avenue_Beach_June_2015_panorama_2.jpg/1280px-Chicago_from_North_Avenue_Beach_June_2015_panorama_2.jpg',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Belgium.svg/1200px-Flag_of_Belgium.svg.png',
         address: 'Some address 10, 12345 Some City',
         description: 'This is a second meetup!',
     },
