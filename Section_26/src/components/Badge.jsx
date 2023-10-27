abd619608ff027cb0cd5cf03d077c015460b9194
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-10-27 20:46:12 +0200

    Finished module 580

--- Section_26/src/components/Badge.jsx
+++ Section_26/src/components/Badge.jsx
@@ -1,3 +1,23 @@
+import { motion } from 'framer-motion'
+
 export default function Badge({ caption }) {
-    return <span className="badge">{caption}</span>
+    return (
+        <motion.span
+            className="badge"
+            animate={{
+                scale: [1, 1.2, 1],
+                rotate: [0, 10, -10, 0],
+                borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+                transition: {
+                    duration: 1.5,
+                    ease: 'easeInOut',
+                    times: [0, 0.2, 0.5, 0.8, 1],
+                    loop: Infinity,
+                    repeatDelay: 1,
+                },
+            }}
+        >
+            {caption}
+        </motion.span>
+    )
 }
